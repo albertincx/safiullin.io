f07ed8e4b5db071ccbaa94827029f0d8c2bd0782
al <albertincx@gmail.com>
2018-06-27 16:03:57 +0300

    tree

---
+++ peerjs/main.js
@@ -0,0 +1,28 @@
+var peer = new Peer();
+
+var conn = peer.connect('safiullin-io-peer-timer');
+// on open will be launch when you successfully connect to PeerServer
+conn.on('open', function(){
+  // here you have conn.id
+  conn.send('hi!');
+});
+
+$(document).ready(function () {
+
+  $('#send').click(function (e) {
+    e.preventDefault()
+    conn.on('open', function(){
+      // here you have conn.id
+      conn.send('hi!');
+    });
+  })
+})
+
+peer.on('connection', function(conn) {
+  conn.on('data', function(data){
+    // Will print 'hi!'
+    $div = $('<div />').text(data);
+    $('#list').append(div)
+    console.log(data);
+  });
+});
